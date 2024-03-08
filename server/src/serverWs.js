f7b4986b18a54f1304f3a3aa6eaeee7404d49d21
Belka <belka.srh.it@gmail.com>
2024-03-08 16:09:24 +0100

    update: ws-server

--- server/src/serverWs.js
+++ server/src/serverWs.js
@@ -10,30 +10,30 @@ const { Elements } = require('./db');
 
 const { PORT_WS = 5000 } = process.env;
 
-// const httpServer = createServer();
-// const io = new Server(httpServer, { cors: { origin: '*' } });
+const httpServer = createServer();
+const io = new Server(httpServer, { cors: { origin: '*' } });
 
-// /* eslint-disable no-console */
-// io.on('connection', socket => {
-//   // add, update element
-//   socket.on('chatMessage', msg => {
-//     socket.broadcast.emit('chatMessage', msg);
-//     const elRef = doc(Elements, msg.id);
-//     setDoc(elRef, msg);
-//   });
-//   // delete element
-//   socket.on('deleteMessage', msg => {
-//     socket.broadcast.emit('deleteMessage', msg);
-//   });
-// });
+/* eslint-disable no-console */
+io.on('connection', socket => {
+  // add, update element
+  socket.on('chatMessage', msg => {
+    socket.broadcast.emit('chatMessage', msg);
+    const elRef = doc(Elements, msg.id);
+    setDoc(elRef, msg);
+  });
+  // delete element
+  socket.on('deleteMessage', msg => {
+    socket.broadcast.emit('deleteMessage', msg);
+  });
+});
 
-// (() => {
-//   try {
-//     httpServer.listen(PORT_WS, () =>
-//       console.log(`  -> Server WS:  http://localhost:${httpServer.address().port}`),
-//     );
-//   } catch (error) {
-//     console.log(error.message);
-//     process.exit(1);
-//   }
-// })();
+(() => {
+  try {
+    httpServer.listen(PORT_WS, () =>
+      console.log(`  -> Server WS:  http://localhost:${httpServer.address().port}`),
+    );
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+})();
