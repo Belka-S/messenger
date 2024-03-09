9698a7de68b1fed67efdc18c747d98e2e07308a8
Belka <belka.srh.it@gmail.com>
2024-03-09 12:28:22 +0100

    update: clean commited code

--- server/src/serverWs.js
+++ server/src/serverWs.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
 
 const { doc, setDoc } = require('firebase/firestore');
 
@@ -8,11 +7,9 @@ const { Elements } = require('./db');
 
 const { PORT_WS = 5000 } = process.env;
 
-const server = express()
-  // .use(cors({ origin: '*' }))
-  .listen(PORT_WS, () => {
-    console.log(`  -> Server ws://localhost:${server.address().port}`);
-  });
+const server = express().listen(PORT_WS, () => {
+  console.log(`  -> Server ws://localhost:${server.address().port}`);
+});
 
 const io = require('socket.io')(server, {
   cors: {
@@ -46,40 +43,3 @@ io.on('connection', socket => {
     socket.broadcast.emit('leftUser', msg);
   });
 });
-
-// const { Server } = require('socket.io');
-
-// // add element
-// const { doc, setDoc } = require('firebase/firestore');
-
-// const { Elements } = require('./db');
-
-// const { PORT_WS = 5000 } = process.env;
-
-// const httpServer = createServer();
-// const io = new Server(httpServer, { cors: { origin: '*' } });
-
-// /* eslint-disable no-console */
-// io.on('connection', socket => {
-//   // add, update element
-//   socket.on('addMessage', msg => {
-//     socket.broadcast.emit('addMessage', msg);
-//     const elRef = doc(Elements, msg.id);
-//     setDoc(elRef, msg);
-//   });
-//   // delete element
-//   socket.on('deleteMessage', msg => {
-//     socket.broadcast.emit('deleteMessage', msg);
-//   });
-// });
-
-// (() => {
-//   try {
-//     httpServer.listen(PORT_WS, () =>
-//       console.log(`  -> Server ws://localhost:${httpServer.address().port}`),
-//     );
-//   } catch (error) {
-//     console.log(error.message);
-//     process.exit(1);
-//   }
-// })();
