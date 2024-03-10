7693d10e9d153ebfc8a3e907009c71dcf32f0ecb
Belka <belka.srh.it@gmail.com>
2024-03-10 11:01:34 +0100

    update: turn on ws-server

--- server/src/serverWs.js
+++ server/src/serverWs.js
@@ -1,23 +1,23 @@
-// require('dotenv').config();
-// const express = require('express');
-// const socket = require('socket.io');
+require('dotenv').config();
+const express = require('express');
+const socket = require('socket.io');
 
-// const onConnection = require('./socket.io/onConnection.js');
+const onConnection = require('./socket.io/onConnection.js');
 
-// const { PORT_WS = 5000 } = process.env;
+const { PORT_WS = 5000 } = process.env;
 
-// const server = express().listen(PORT_WS, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`  -> Server ws://localhost:${server.address().port}`);
-// });
+const server = express().listen(PORT_WS, () => {
+  // eslint-disable-next-line no-console
+  console.log(`  -> Server ws://localhost:${server.address().port}`);
+});
 
-// const io = socket(server, {
-//   cors: {
-//     origins: '*:*',
-//     methods: ['GET', 'POST'],
-//   },
-// });
+const io = socket(server, {
+  cors: {
+    origins: '*:*',
+    methods: ['GET', 'POST'],
+  },
+});
 
-// io.on('connection', socket => {
-//   onConnection(socket);
-// });
+io.on('connection', socket => {
+  onConnection(socket);
+});
