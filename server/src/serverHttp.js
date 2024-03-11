1b54f20050d72665764fb4e29e0b8b2eec525701
Belka <belka.srh.it@gmail.com>
2024-03-11 08:20:58 +0100

    turn off http server

--- server/src/serverHttp.js
+++ server/src/serverHttp.js
@@ -5,12 +5,12 @@ const app = require('./app');
 const { PORT_HTTP = 4000 } = process.env;
 
 /* eslint-disable no-console */
-(() => {
-  try {
-    // console.info('  -> Connected to Firebase!');
-    app.listen(PORT_HTTP, () => console.log(`  -> Server http://localhost:${PORT_HTTP}`));
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
-})();
+// (() => {
+//   try {
+//     // console.info('  -> Connected to Firebase!');
+//     app.listen(PORT_HTTP, () => console.log(`  -> Server http://localhost:${PORT_HTTP}`));
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// })();
